f8e9cfde42f6f5c15c491c195dc3b6316003dc5e
edd <zeedymarina@gmail.com>
2021-01-06 20:18:26 -0500

    loja

--- src/brickBall/MainPanel.java
+++ src/brickBall/MainPanel.java
@@ -1,5 +1,17 @@
 package brickBall;
 
+import javax.swing.*;
+
 public class MainPanel {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        gamePlay gameplay = new gamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Break the Wall");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
 
 }
